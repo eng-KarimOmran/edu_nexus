878cdda51f3d4a9b48a5db960b49ef40174afa1d
Karim Omran <eng.karimomran@gmail.com>
2026-07-08 21:48:30 +0300

    BASE_URL

--- dist/modules/auth/auth.utils.js
+++ dist/modules/auth/auth.utils.js
@@ -1,58 +1,21 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.cookieAccess = exports.cookieRefresh = exports.Token = exports.HashHelper = void 0;
-const argon2 = __importStar(require("argon2"));
 const nanoid_1 = require("nanoid");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const env_1 = __importDefault(require("../../config/env"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 exports.HashHelper = {
     async hash(password) {
-        return argon2.hash(password, {
-            type: argon2.argon2id,
-            memoryCost: 2 ** 16,
-            timeCost: 4,
-            parallelism: 2,
-        });
+        const saltRounds = 12;
+        return await bcrypt_1.default.hash(password, saltRounds);
     },
-    async compare({ plainPassword, hashedPassword }) {
+    async compare({ plainPassword, hashedPassword, }) {
         try {
-            return await argon2.verify(hashedPassword, plainPassword);
+            return await bcrypt_1.default.compare(plainPassword, hashedPassword);
         }
         catch (error) {
             console.error("HashHelper compare error:", error);

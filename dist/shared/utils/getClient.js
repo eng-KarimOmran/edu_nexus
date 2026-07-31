f767124dde031a357e3863f29429db6f3f70c2d7
Karim Omran <eng.karimomran@gmail.com>
2026-07-31 16:54:45 +0300

    schema

--- dist/shared/utils/getClient.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("../../lib/prisma");
-const getClient = (tx) => tx ?? prisma_1.prisma;
-exports.default = getClient;
